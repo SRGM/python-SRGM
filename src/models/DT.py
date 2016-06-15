d9121848894042a2c802cbe70521bb00805eb9b5
arhik <karthik.katipalli@gmail.com>
2016-06-15 00:15:36 -0400

    Working DT.py

--- src/models/DT.py
+++ src/models/DT.py
@@ -75,24 +75,32 @@ class DecisionTree(Tree):
     def predict(self, x, node=None):
         if not node:
             node = self.root
-        if not node.threshold:
-            print("Node do not have threshold")
-            print(node.name)
-            print(node.parent)
-            return(3)
-        if node.threshold >= x:
-            if node.left:
-                node = node.left
+            print("Root is Used")
+        if node.threshold:
+            if node.threshold >=x:
+                if node.left:
+                    print("left exists")
+                    node = node.left
+                    return(self.predict(x,node))
+                else:
+                    return(node.parent.threshold)
+            elif node.threshold < x:
+                if node.right:
+                    print("right exists")
+                    node = node.right
+                    return(self.predict(x,node))
+                else:
+                    return(node.parent.threshold)
             else:
-                return(node.threshold)
+                print("Error")
         else:
-            if node.right:
-                node = node.left
+            print("threshold do not exist")
+            print(node.parent.threshold)
+            if node.parent.threshold:
+                print("parent threshold exists")
             else:
-                return(node.threshold)
-            node = node.right
-        if node:
-            self.predict(x,node)
+                print("Alas")
+            return(node.parent.threshold)
 
 
 x = [3, 33, 146, 227, 342, 351, 353, 444, 556, 571, 709, 759, 836, 860, 968, 1056, 1726, 1846, 1872, 1986, 2311, 2366, 2608, 2676, 3098, 3278, 3288, 4434, 5034, 5049, 5085, 5089, 5089, 5097, 5324, 5389, 5565, 5623, 6080, 6380, 6477, 6740, 7192, 7447, 7644, 7837, 7843, 7922, 8738, 10089, 10237, 10258, 10491, 10625, 10982, 11175, 11411, 11442, 11811, 12559, 12559, 12791, 13121, 13486, 14708, 15251, 15261, 15277, 15806, 16185, 16229, 16358, 17168, 17458, 17758, 18287, 18568, 18728, 19556, 20567, 21012, 21308, 23063, 24127, 25910, 26770, 27753, 28460, 28493, 29361, 30085, 32408, 35338, 36799, 37642, 37654, 37915, 39715, 40580, 42015, 42045, 42188, 42296, 42296, 45406, 46653, 47596, 48296, 49171, 49416, 50145, 52042, 52489, 52875, 53321, 53443, 54433, 55381, 56463, 56485, 56560, 57042, 62551, 62651, 62661, 63732, 64103, 64893, 71043, 74364, 75409,76057, 81542, 82702, 84566, 88682]
@@ -100,7 +108,9 @@ x = [3, 33, 146, 227, 342, 351, 353, 444, 556, 571, 709, 759, 836, 860, 968, 105
 dt = DecisionTree(x, max_depth = 10)
 
 import numpy as np
+import matplotlib.pyplot as plt
 y = np.arange(len(x))
 plt.plot(x,y)
 
 pred_y = np.asarray([dt.predict(i) for i in x])
+plt.plot(pred_y,y)
