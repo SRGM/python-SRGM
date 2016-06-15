b982d1781ba2423d6479d9f00ce03e7d1f22572b
arhik <karthik.katipalli@gmail.com>
2016-06-15 16:39:09 -0400

    DT complete implementation for Failure Time data

--- src/models/DT.py
+++ src/models/DT.py
@@ -12,6 +12,7 @@ class Node:
         self.parent = None
         self.name = name
         self.threshold = None
+        self.value = None
 
     def add_left(self, node_obj):
         print("Node {1} is added as left child to Node {0}".format(self.name, node_obj.name))
@@ -59,12 +60,15 @@ class DecisionTree(Tree):
         self.compute(x, root,0)
     def compute(self,x ,parent ,depth):
         import numpy as np
-        threshold = np.mean(x)
-        print(threshold)
+        threshold = np.mean([i[0] for i in x])
+        value = np.mean([i[1] for i in x])
+        print("Threshold: {0}".format(threshold))
+        print("Value: {0}".format(value))
         parent.threshold = threshold
+        parent.value = value
         if len(x) <= 2 or depth >= self.max_depth:
             return
-        threshold_index =  [1 if xi >= threshold else 0 for i,xi in enumerate(x)].index(1)
+        threshold_index =  [1 if xi >= threshold else 0 for xi,i in x].index(1)
         x_left = x[:threshold_index]
         x_right = x[threshold_index:]
         depth += 1
@@ -87,36 +91,41 @@ class DecisionTree(Tree):
                 if node.left:
                     print("left exists")
                     node = node.left
+                    
                     return(self.predict(x,node))
                 else:
-                    return(node.parent.threshold)
+                    print("Threshold: {0}".format(node.parent.threshold))
+                    print("Value: {0}".format(node.parent.value))
+                    return(node.parent.value)
             elif node.threshold < x:
                 if node.right:
                     print("right exists")
                     node = node.right
                     return(self.predict(x,node))
                 else:
-                    return(node.parent.threshold)
+                    print("Threshold: {0}".format(node.parent.threshold))
+                    print("Value: {0}".format(node.parent.value))
+                    return(node.parent.value)
             else:
                 print("Error")
         else:
             print("threshold do not exist")
-            print(node.parent.threshold)
+            print(node.parent.value)
             if node.parent.threshold:
                 print("parent threshold exists")
             else:
                 print("Alas")
-            return(node.parent.threshold)
+            return(node.parent.value)
 
 
-x = [3, 33, 146, 227, 342, 351, 353, 444, 556, 571, 709, 759, 836, 860, 968, 1056, 1726, 1846, 1872, 1986, 2311, 2366, 2608, 2676, 3098, 3278, 3288, 4434, 5034, 5049, 5085, 5089, 5089, 5097, 5324, 5389, 5565, 5623, 6080, 6380, 6477, 6740, 7192, 7447, 7644, 7837, 7843, 7922, 8738, 10089, 10237, 10258, 10491, 10625, 10982, 11175, 11411, 11442, 11811, 12559, 12559, 12791, 13121, 13486, 14708, 15251, 15261, 15277, 15806, 16185, 16229, 16358, 17168, 17458, 17758, 18287, 18568, 18728, 19556, 20567, 21012, 21308, 23063, 24127, 25910, 26770, 27753, 28460, 28493, 29361, 30085, 32408, 35338, 36799, 37642, 37654, 37915, 39715, 40580, 42015, 42045, 42188, 42296, 42296, 45406, 46653, 47596, 48296, 49171, 49416, 50145, 52042, 52489, 52875, 53321, 53443, 54433, 55381, 56463, 56485, 56560, 57042, 62551, 62651, 62661, 63732, 64103, 64893, 71043, 74364, 75409,76057, 81542, 82702, 84566, 88682]
-
-dt = DecisionTree(x, max_depth = 10)
+x_data = [3, 33, 146, 227, 342, 351, 353, 444, 556, 571, 709, 759, 836, 860, 968, 1056, 1726, 1846, 1872, 1986, 2311, 2366, 2608, 2676, 3098, 3278, 3288, 4434, 5034, 5049, 5085, 5089, 5089, 5097, 5324, 5389, 5565, 5623, 6080, 6380, 6477, 6740, 7192, 7447, 7644, 7837, 7843, 7922, 8738, 10089, 10237, 10258, 10491, 10625, 10982, 11175, 11411, 11442, 11811, 12559, 12559, 12791, 13121, 13486, 14708, 15251, 15261, 15277, 15806, 16185, 16229, 16358, 17168, 17458, 17758, 18287, 18568, 18728, 19556, 20567, 21012, 21308, 23063, 24127, 25910, 26770, 27753, 28460, 28493, 29361, 30085, 32408, 35338, 36799, 37642, 37654, 37915, 39715, 40580, 42015, 42045, 42188, 42296, 42296, 45406, 46653, 47596, 48296, 49171, 49416, 50145, 52042, 52489, 52875, 53321, 53443, 54433, 55381, 56463, 56485, 56560, 57042, 62551, 62651, 62661, 63732, 64103, 64893, 71043, 74364, 75409,76057, 81542, 82702, 84566, 88682]
+d = list(zip(x_data,list(range(1,len(x_data)+1))))
+dt = DecisionTree(d, max_depth = 10)
 
 import numpy as np
 import matplotlib.pyplot as plt
-y = np.arange(len(x))
-plt.plot(x,y)
+y = np.arange(len(x_data))
+plt.plot(x_data,y)
 
-pred_y = np.asarray([dt.predict(i) for i in x])
-plt.plot(pred_y,y)
+pred_y = np.asarray([dt.predict(i) for i,y in d])
+plt.plot(x_data,pred_y)
